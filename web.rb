d831205190d6163a599427fb997a4f724cccc251
bati11 <mail.bati11@gmail.com>
2014-08-20 08:48:50 +0900

    Implement status page

--- web.rb
+++ web.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'erb'
 
 class App < Sinatra::Base
   configure :development do
@@ -8,7 +9,43 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    "Hello World!"
+    twitter_variables = %w[
+      TWITTER_CONSUMER_KEY
+      TWITTER_CONSUMER_SECRET
+      TWITTER_ACCESS_TOKEN
+      TWITTER_ACCESS_TOKEN_SECRET
+    ]
+    twitter_list = twitter_variables.map {|x|
+      {
+        name: x,
+        status: !ENV[x].nil?
+      }
+    }
+
+    github_variables = %w[
+      GITHUB_OAUTH_TOKEN
+      GITHUB_USER
+    ]
+    github_list = github_variables.map {|x|
+      {
+        name: x,
+        status: !ENV[x].nil?
+      }
+    }
+
+    redis_list = [
+      {
+        name: "REDISTOGO_URL",
+        status: !ENV["REDISTOGO_URL"].nil?
+      }
+    ]
+
+    @list = [
+      {label: "Twitter", list: twitter_list},
+      {label: "GitHub", list: github_list},
+      {label: "Redis", list: redis_list}
+    ]
+    erb :index
   end
 end
 
